7de966e531127b2cc8979c397ae7fd34f23be1b6
Siren <siren@kernal.eu>
2021-07-18 23:33:44 +0300

    Create struct for the get_transfer_by_txid method

--- walletrpc/common.go
+++ walletrpc/common.go
@@ -71,50 +71,6 @@ type Payment struct {
 	Address string `json:"address"`
 }
 
-type Transfer struct {
-	// Public address of the transfer.
-	Address string `json:"address"`
-
-	// Amount of this transfer.
-	Amount uint64 `json:"amount"`
-
-	// Number of blocks mined since the block containing this transaction (or block height at which the transaction should be added to a block if not yet confirmed).
-	Confirmations uint64 `json:"confirmations"`
-
-	// True if the key image(s) for the transfer have been seen before.
-	DoubleSpendSeen bool `json:"double_spend_seen"`
-
-	// Transaction fee for this transfer.
-	Fee uint64 `json:"fee"`
-
-	// Height of the first block that confirmed this transfer (0 if not mined yet).
-	Height uint64 `json:"height"`
-
-	// Note about this transfer.
-	Note string `json:"note"`
-
-	// Payment ID for this transfer.
-	PaymentId string `json:"payment_id"`
-
-	// Account and subaddress index.
-	SubaddrIndex SubaddressIndex `json:"subaddr_index"`
-
-	// Estimation of the confirmations needed for the transaction to be included in a block.
-	SuggestedConfirmationsThreshold uint64 `json:"suggested_confirmations_threshold"`
-
-	// POSIX timestamp for when this transfer was first confirmed in a block (or timestamp submission if not mined yet).
-	Timestamp uint64 `json:"timestamp"`
-
-	// Transaction ID for this transfer.
-	Txid string `json:"txid"`
-
-	// Transfer type.
-	Type string `json:"type"`
-
-	// Number of blocks until transfer is safely spendable.
-	UnlockTime uint64 `json:"unlock_time"`
-}
-
 type SignedKeyImage struct {
 	KeyImage  string `json:"key_image"`
 	Signature string `json:"signature"`
